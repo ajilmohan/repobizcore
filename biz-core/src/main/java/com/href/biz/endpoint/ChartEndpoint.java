3a82755bd24829689eeed9800e6081f163c2d117
Ajil <ajilam@gmail.com>
2015-04-27 22:04:48 +0530

    Updated chart service.

--- biz-core/src/main/java/com/href/biz/endpoint/ChartEndpoint.java
+++ biz-core/src/main/java/com/href/biz/endpoint/ChartEndpoint.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -13,8 +14,13 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.href.biz.domain.DicnShow;
+import com.href.biz.domain.Movie;
+import com.href.biz.domain.Screen;
+import com.href.biz.domain.Show;
 import com.href.biz.dto.DicnShowDTO;
 import com.href.biz.dto.MovieDTO;
+import com.href.biz.dto.ShowDTO;
 import com.href.biz.proxy.DicnShowProxy;
 import com.href.biz.proxy.MovieProxy;
 import com.href.biz.proxy.ShowProxy;
@@ -44,10 +50,10 @@ public class ChartEndpoint {
 	 * @return 
 	 */
 	
-	@RequestMapping(value = "/cinema/chart/shows", method = RequestMethod.GET)
+	@RequestMapping(value = "/cinema/chart/getshows", method = RequestMethod.GET)
 	@ResponseBody
 	public ResponseEntity<List<DicnShowDTO>> getShowsForCharting(){
-		logger.info("Endpoint .getShowsForCharting() '/cinema/chart/shows invoked . ");
+		logger.info("Endpoint .getShowsForCharting() '/cinema/chart/getshows invoked . ");
 		List<DicnShowDTO>  shows  = new ArrayList<DicnShowDTO>();
 		try {
 			shows = dicnShowProxy.getAllShows();
@@ -66,10 +72,10 @@ public class ChartEndpoint {
 	 * @return 
 	 */
 	
-	@RequestMapping(value = "/cinema/chart/movies", method = RequestMethod.GET )
+	@RequestMapping(value = "/cinema/chart/getmovies", method = RequestMethod.GET )
 	@ResponseBody
 	public ResponseEntity<List<MovieDTO>> getMoviesForCharting(){
-		logger.info("Endpoint .getMoviesForCharting() '/cinema/chart/movies invoked . ");
+		logger.info("Endpoint .getMoviesForCharting() '/cinema/chart/getmovies invoked . ");
 		List<MovieDTO>  movies  = new ArrayList<MovieDTO>();
 		try {
 			movies = movieProxy.getAllActiveMovies();
@@ -81,5 +87,39 @@ public class ChartEndpoint {
 		return new ResponseEntity<List<MovieDTO>>(movies, HttpStatus.OK);
 		
 	}
+	
+	@RequestMapping(value = "/cinema/chart/show", method = RequestMethod.PUT , consumes = {"application/json"} )
+	@ResponseBody
+	public ResponseEntity<String> chartShow(ShowDTO showDto){
+		logger.info("Endpoint .chartShow() '/cinema/chart/show invoked . ");
+		try {
+			showProxy.save(convertFromShowDtoToShow(showDto));
+		} catch (Exception e) {
+			logger.error("Error !!!");
+			logger.error("Error : 'Exception' " + e.getMessage());
+		}
+		logger.info("Exit . ");
+		return new ResponseEntity<String>("Show Charted", HttpStatus.OK);
+	}
+	
+	
+	private Show convertFromShowDtoToShow(ShowDTO showDto){
+		logger.info("Endpoint .convertFromShowDtoToShow()  invoked . ");
+		String[] showPropIgnore = {"dicnShow","movie","screen"};
+		Show show  = new Show();
+		BeanUtils.copyProperties(showDto, show,showPropIgnore);
+		DicnShow dicShow  = new DicnShow();
+		dicShow.setId(showDto.getDicnShow().getId());
+		show.setDicnShow(dicShow);
+		Movie movie = new Movie();
+		movie.setId(showDto.getMovie().getId());
+		show.setMovie(movie);
+		Screen screen = new Screen();
+		screen.setId(showDto.getScreen().getId());
+		show.setScreen(screen);
+		logger.info("Exit . ");
+		return show;
+	}
+	
 
 }
